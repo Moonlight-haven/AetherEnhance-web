5d139653b41753a441fbcaaf160ef5c527a5129c
Moonlight-haven <moonlight.allacct@gmail.com>
2026-08-06 15:03:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 const AETHER_API_CONFIG = {
   CLIENT_KEY: "awm0jgm4w8o7m0vg",
   REDIRECT_URI: "https://aetherenhance-web.vercel.app/studio.html",
-  SERVER_API_ROOT: "https://aether-backend-engine.onrender.com/api"
+  SERVER_API_ROOT: "/api"
 };
 window.AETHER_API_CONFIG = AETHER_API_CONFIG;
